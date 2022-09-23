102981cdc3b59247ec6e746cab2a1cf0a7c9b827
Matheus <matheuslf44@gmail.com>
2022-09-23 10:24:13 -0400

    first commit

--- purchases/src/http/auth/authorization.guard.ts
+++ purchases/src/http/auth/authorization.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { expressjwt } from 'express-jwt';
+import { expressjwt, GetVerificationKey } from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
 
@@ -26,7 +26,7 @@ export class AuthorizationGuard implements CanActivate {
           rateLimit: true,
           jwksRequestsPerMinute: 5,
           jwksUri: `${this.AUTH0_DOMAIN}/.well-known/jwks.json`,
-        }),
+        }) as GetVerificationKey,
         audience: this.AUTH0_AUDIENCE,
         issuer: `https://${this.AUTH0_DOMAIN}/`,
         algorithms: ['RS256'],
